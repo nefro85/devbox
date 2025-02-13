9ff148c482190741b79f7f3cff3bd127deb2a14b
mario <mariusz.sygnowski@gmail.com>
2025-02-13 12:55:04 +0100

    kafka support

--- py/meshtastic/dig_radio_data.py
+++ py/meshtastic/dig_radio_data.py
@@ -3,42 +3,60 @@ import time
 import logging
 import base64
 import datetime
+
 from pubsub import pub
+from kafka import KafkaProducer
 
 
 import meshtastic
 import meshtastic.serial_interface
 
-def onReceive(packet, interface, topic=pub.AUTO_TOPIC):  # pylint: disable=unused-argument
-    """called when a packet arrives"""
-    print("==============")
-    stamp = datetime.datetime.now().astimezone().isoformat()
-    
-    keys_list = list(packet.keys())
-    for key in keys_list:
-        if key == 'raw':
-            bin: bytes = packet['raw'].SerializeToString()
-            print(f"[BASE64]{stamp},{base64.b64encode(bin).decode('utf-8')}")
-            # hex = packet['raw'].SerializeToString().hex()
-            # print(f"[HEX]: {hex}")
-        logging.info(f"{key} : {type(packet[key])} => {packet[key]}")
-    
-    #print(f"Received: {hex}")
+
+class MeshHandler():
+    def __init__(self, useKafka:bool = None, topicMesh="meshtastic-from-radio"):
+        
+        self.useKafka = useKafka
+        self.topicName = topicMesh
+        self.device_id = None
+
+        if useKafka:
+            self.producer = KafkaProducer(bootstrap_servers='lab.syg:9092')
+
+    def onReceive(self, packet, interface, topic=pub.AUTO_TOPIC):  # pylint: disable=unused-argument
+        """called when a packet arrives"""
+        print("==============")
+        stamp = datetime.datetime.now().astimezone().isoformat()
+        
+        keys_list = list(packet.keys())
+        for key in keys_list:
+            if key == 'raw':
+                bin: bytes = packet['raw'].SerializeToString()
+                
+                if self.useKafka:
+                    self.producer.send(self.topicName, bin)
+                
+                print(f"[BASE64]{stamp},{base64.b64encode(bin).decode('utf-8')}")
+                # hex = packet['raw'].SerializeToString().hex()
+                # print(f"[HEX]: {hex}")
+            logging.info(f"{key} : {type(packet[key])} => {packet[key]}")
+        
+        #print(f"Received: {hex}")
 
 
-def onConnection(interface, topic=pub.AUTO_TOPIC):  # pylint: disable=unused-argument
-    """called when we (re)connect to the radio"""
-    # defaults to broadcast, specify a destination ID if you wish
-    print("connected")
-
+    def onConnection(self, interface, topic=pub.AUTO_TOPIC):  # pylint: disable=unused-argument
+        """called when we (re)connect to the radio"""
+        # defaults to broadcast, specify a destination ID if you wish
+        print("connected")
 
 
 
 def main():
     logging.basicConfig(level=logging.WARN)
 
-    pub.subscribe(onReceive, "meshtastic.receive")
-    pub.subscribe(onConnection, "meshtastic.connection.established")
+    meshHandler = MeshHandler(useKafka=True)
+
+    pub.subscribe(meshHandler.onReceive, "meshtastic.receive")
+    pub.subscribe(meshHandler.onConnection, "meshtastic.connection.established")
 
     try:
         iface = meshtastic.serial_interface.SerialInterface()
@@ -46,9 +64,16 @@ def main():
         if iface.nodes:
             for n in iface.nodes.values():
                 if n["num"] == iface.myInfo.my_node_num:
-                    print(n["user"]["hwModel"])
-                    break
+                    dev_num = n["num"]
+                    dev_user = n["user"]["shortName"]
+                    dev_model = n["user"]["hwModel"]
+                    
+                    device_id = f"{dev_num}/{dev_user}/{dev_model}"
 
+                    meshHandler.device_id = device_id
+
+                    print(f"Device: {device_id}")
+                    break
         
         try:
             while True:
